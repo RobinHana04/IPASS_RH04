29f9744cc82449de9663f277a201341bc0b09232
RobinHana04 <robinhana2004@gmail.com>
2023-06-08 18:46:30 +0200

    Added more tests, moved some files around for better directory structure, made some domain method tweaks

--- src/main/java/Persistence/PersistenceManager.java
+++ src/main/java/Persistence/PersistenceManager.java
@@ -1,45 +1,50 @@
 package Persistence;
 
-import org.backend.requests.VacationRental;
 import org.backend.domain.Vakantiehuis;
 import org.backend.domain.Verhuurder;
+import org.backend.requests.VacationRental;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 public class PersistenceManager {
-    static Path filePath = Path.of("C:\\Users\\Robin Hana\\IPASS_VacationRental\\src\\main\\java\\Persistence\\VacationRental.obj");
+    private static final String filePath = "VacationRental.obj";
+    private static final File file = new File(filePath);
 
     public static void loadVacationRentalFromFile() throws IOException, ClassNotFoundException {
-        InputStream is = Files.newInputStream(filePath);
-        ObjectInputStream ois = new ObjectInputStream(is);
-        Object obj = ois.readObject();
-        if(obj != null) {
-            VacationRental loadedObj = (VacationRental) obj;
-            List<Vakantiehuis> vakantiehuizen = loadedObj.getAllVakantieHuizen();
-            List<Verhuurder> verhuurders = loadedObj.getAllVerhuurders();
+        if (file.exists()) {
+            FileInputStream is = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(is);
+            Object obj = ois.readObject();
 
-            System.out.println("Vakantiehuizen:");
-            for (Vakantiehuis vakantiehuis : vakantiehuizen) {
-                System.out.println(vakantiehuis);
-            }
+            if (obj != null) {
+                VacationRental loadedObj = (VacationRental) obj;
+                List<Vakantiehuis> vakantiehuizen = loadedObj.getAllVakantieHuizen();
+                List<Verhuurder> verhuurders = loadedObj.getAllVerhuurders();
 
-            System.out.println("Verhuurders:");
-            for (Verhuurder verhuurder : verhuurders) {
-                System.out.println(verhuurder);
+                System.out.println("Vakantiehuizen:");
+                for (Vakantiehuis vakantiehuis : vakantiehuizen) {
+                    System.out.println(vakantiehuis);
+                }
+
+                System.out.println("Verhuurders:");
+                for (Verhuurder verhuurder : verhuurders) {
+                    System.out.println(verhuurder);
+                }
+                VacationRental.setVacationRental(loadedObj);
+                ois.close();
             }
-            VacationRental.setVacationRental(loadedObj);
-            ois.close();
+        } else {
+            System.out.println("File bestaat niet.");
         }
     }
 
     public static void saveVacationRentalToFile() throws IOException {
         VacationRental vcr = VacationRental.getVacationRental();
-        OutputStream os = Files.newOutputStream(filePath);
+        OutputStream os = new FileOutputStream(file);
         ObjectOutputStream oos = new ObjectOutputStream(os);
         oos.writeObject(vcr);
+        System.out.println("Alle verhuurders van VR object: " + vcr.getVakantiehuizenVR());
         oos.flush();
         oos.close();
     }
--- src/main/java/setup/MyServletContextListener.java
+++ src/main/java/setup/MyServletContextListener.java
@@ -1,9 +1,6 @@
 package setup;
 
 import Persistence.PersistenceManager;
-import org.backend.requests.VacationRental;
-import org.backend.domain.Vakantiehuis;
-import org.backend.domain.Verhuurder;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -17,20 +14,7 @@ public class MyServletContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         try {
             System.out.println("Server is starting with necessary data");
-            Verhuurder v1 = new Verhuurder("Robin");
-            Vakantiehuis vh1 = new Vakantiehuis("Huisje 1", "Victoriameer 121", "50m2", 1);
-           Vakantiehuis vh2 = new Vakantiehuis("Huisje 2", "AmeliaPolder 33", "220m2", 2);
-           Vakantiehuis.addHuis(vh1);
-           Vakantiehuis.addHuis(vh2);
-           v1.voegHuisToe(vh1);
-           v1.voegHuisToe(vh2);
-           Verhuurder.voegVerhuurderToe(v1);
-
-           VacationRental vacationRental = VacationRental.getVacationRental();
-           vacationRental.addVakantiehuizenVR(vh1);
-           vacationRental.addVakantiehuizenVR(vh2);
-           vacationRental.addVerhuurderVR(v1);
-            PersistenceManager.loadVacationRentalFromFile();
+           PersistenceManager.loadVacationRentalFromFile();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -43,7 +27,7 @@ public class MyServletContextListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         try {
-            System.out.println("Terminating application");
+            System.out.println("Terminating application: ");
             PersistenceManager.saveVacationRentalToFile();
         } catch (IOException e) {
             throw new RuntimeException(e);
